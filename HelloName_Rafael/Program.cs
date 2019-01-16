054072df8747dc030021bc09ecb7065d1d20b4ab
R Res <clonnes@hotmail.com>
2019-01-15 19:15:15 -0500

    Add project files.

---
+++ HelloName_Rafael/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloName_Rafael
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
